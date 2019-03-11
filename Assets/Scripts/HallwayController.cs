8c90493882bf5f9c477c2cac279726c2e41f7171
Unknown <aea313@nyu.edu>
2019-03-11 18:38:34 -0400

    add raymarching toolkit and ray hallway

--- Assets/Scripts/HallwayController.cs
+++ Assets/Scripts/HallwayController.cs
@@ -15,20 +15,15 @@ public class HallwayController : MonoBehaviour
 
     void Start()
     {
-        SpawnHallway();
-        // corner.transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
-    }
-
-    void SpawnHallway()
-    {
-        // leftWall  = Instantiate(prefab);
-        // rightWall = Instantiate(prefab);
-        // floor 	  = Instantiate(prefab);
-
-		leftWall.transform.parent = transform;
+        leftWall.transform.parent = transform;
 		rightWall.transform.parent = transform;
 		floor.transform.parent = transform;
+        SetHallwayInfo();
+        UpdateCorner();
+    }
 
+    void SetHallwayInfo()
+    {
 
         leftWall .transform.localScale = new Vector3(wallWidth, wallHeight, hallLength);
         rightWall.transform.localScale = new Vector3(wallWidth, wallHeight, hallLength);
@@ -37,7 +32,11 @@ public class HallwayController : MonoBehaviour
         leftWall .transform.localPosition = new Vector3(-hallWidth / 2, wallHeight / 2, 0);
         rightWall.transform.localPosition = new Vector3( hallWidth / 2, wallHeight / 2, 0);
         floor	 .transform.localPosition = new Vector3(0, 0, 0);
-        
+    }
+
+    void UpdateCorner() {
+        corner.crnrWidth = hallWidth;
+        corner.transform.localPosition = new Vector3(0, 0, hallLength / 2 + hallWidth / 2);
     }
 
     public GameObject leftWall, rightWall, floor;
@@ -52,6 +51,7 @@ public class HallwayController : MonoBehaviour
         rightWall.transform.localPosition = new Vector3( hallWidth / 2, wallHeight / 2, 0);
         floor	 .transform.localPosition = new Vector3(0, 0, 0);
 
-        corner.transform.localPosition = new Vector3(0, 0, hallLength / 2 + hallWidth / 2);
+
+        UpdateCorner();
     }
 }
