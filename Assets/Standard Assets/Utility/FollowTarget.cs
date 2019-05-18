6fa1a71b2361af45b3f31e4c6ceab29c75ea8714
Unknown <aea313@nyu.edu>
2019-05-18 19:20:42 -0400

    improve collider tracking on headset

--- Assets/Standard Assets/Utility/FollowTarget.cs
+++ Assets/Standard Assets/Utility/FollowTarget.cs
@@ -9,10 +9,19 @@ namespace UnityStandardAssets.Utility
         public Transform target;
         public Vector3 offset = new Vector3(0f, 7.5f, 0f);
 
+        public bool keepGrounded;
+        public float groundHeight;
+
         private void LateUpdate()
         {
             if(target == null) { return; }
             transform.position = target.position + offset;
+
+            if(keepGrounded) {
+                Vector3 temp = transform.position; 
+                temp.y = groundHeight;
+                transform.position = temp;
+            }
         }
     }
 }
